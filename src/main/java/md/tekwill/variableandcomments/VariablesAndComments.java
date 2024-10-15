7fe1b10795308e06ffefa4821e5348344f2794f7
anac <assurancequality430@gmail.com>
2024-10-15 13:59:15 +0300

    Added the Variables and Comments task

---
+++ src/main/java/md/tekwill/variableandcomments/VariablesAndComments.java
@@ -0,0 +1,63 @@
+package md.tekwill.variableandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+        //Declare the primitive data type variables
+
+        boolean trueBooleanOfVariable;
+        boolean falseBooleanOfVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Intialization of the above variables
+
+        trueBooleanOfVariable = true;
+        falseBooleanOfVariable = false;
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305d;
+        firstCharFromAscii = 'A';
+        lastCharFromAscii = '#';
+
+        System.out.println(trueBooleanOfVariable);
+        System.out.println(falseBooleanOfVariable);
+        System.out.println(lowerLimitOfByteDataType);
+        System.out.println(upperLimitOfByteDataType);
+        System.out.println(lowerLimitOfShortDataType);
+        System.out.println(upperLimitOfShortDataType);
+        System.out.println(lowerLimitOfIntDataType);
+        System.out.println(upperLimitOfIntDataType);
+        System.out.println(lowerLimitOfLongDataType);
+        System.out.println(upperLimitOfLongDataType);
+        System.out.println(lowerLimitOfFloatDataType);
+        System.out.println(upperLimitOfFloatDataType);
+        System.out.println(lowerLimitOfDoubleDataType);
+        System.out.println(upperLimitOfDoubleDataType);
+        System.out.println(firstCharFromAscii);
+        System.out.println(lastCharFromAscii);
+
+    }
+}
